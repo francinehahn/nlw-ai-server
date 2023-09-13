5dbeab5341ab47a395df15347c3a895fa47a7cfb
Francine Hahn <fran_hahn@hotmail.com>
2023-09-13 00:33:58 -0300

    feat: create transcription and generate ai completion endpoints

--- src/routes/upload-video.ts
+++ src/routes/upload-video.ts
@@ -36,10 +36,10 @@ export async function uploadVideoRoute(app: FastifyInstance) {
     await pump(data.file, fs.createWriteStream(uploadDestination))
 
     const video = await prisma.video.create({
-        data: {
-          name: data.filename,
-          path: uploadDestination
-        }
+      data: {
+        name: data.filename,
+        path: uploadDestination
+      }
     })
 
     return {video}
